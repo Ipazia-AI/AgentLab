23a564776b493295379972a37689122636e12706
Gianni Pellegrini <g.pellegrini@ipazia.com>
2026-03-03 12:42:49 +0100

    revised hpa experiment script

--- tutorials/4_hpa_on_workarena/experiment_hpa.py
+++ tutorials/4_hpa_on_workarena/experiment_hpa.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from bgym import DEFAULT_BENCHMARKS
 from dotenv import load_dotenv
 
-import agentlab.agents.dynamic_prompting as dp
-from agentlab.agents.generic_agent.generic_agent_prompt import GenericPromptFlags
+from agentlab.agents.generic_agent.tmlr_config import BASE_FLAGS
 from agentlab.agents.structured_agent.hpa_agent import HPAAgentArgs
 from agentlab.agents.structured_agent.hpa_prompt import HPAPromptFlags
 from agentlab.experiments.study import Study
@@ -13,44 +12,6 @@ from agentlab.llm.chat_api import OpenRouterModelArgs
 # Load environment variables for WorkArena (e.g., ServiceNow credentials/instance).
 project_dir = Path(__file__).parents[2]
 load_dotenv(project_dir.joinpath(".env"), override=False)
-
-prompt_flags = GenericPromptFlags(
-    obs=dp.ObsFlags(
-        use_html=False,
-        use_ax_tree=True,
-        use_focused_element=True,
-        use_error_logs=True,
-        use_history=True,
-        use_past_error_logs=False,
-        use_action_history=True,
-        use_think_history=True,
-        use_diff=False,
-        html_type="pruned_html",
-        use_screenshot=False,
-        use_som=False,
-        extract_visible_tag=True,
-        extract_clickable_tag=True,
-        extract_coords="False",
-        filter_visible_elements_only=False,
-    ),
-    action=dp.ActionFlags(
-        multi_actions=False,
-        action_set="bid",
-        long_description=False,
-        individual_examples=False,
-    ),
-    use_plan=False,
-    use_criticise=False,
-    use_thinking=True,
-    use_memory=False,
-    use_concrete_example=True,
-    use_abstract_example=True,
-    use_hints=True,
-    enable_chat=False,
-    max_prompt_tokens=40_000,
-    be_cautious=True,
-    extra_instructions=None,
-)
 
 max_total_tokens = 1000 # Change values here!!!!!!
 max_new_tokens = 100 # Change values here!!!!!
@@ -65,22 +26,22 @@ chat_model_args = OpenRouterModelArgs(
 )
 
 hpa_prompt_flags = HPAPromptFlags(
-    obs=prompt_flags.obs,
-    action=prompt_flags.action,
+    obs=BASE_FLAGS.obs,
+    action=BASE_FLAGS.action,
     use_constraints=True,
     use_progress=True,
     use_suggestion=True,
-    use_plan=prompt_flags.use_plan,
-    use_criticise=prompt_flags.use_criticise,
-    use_thinking=prompt_flags.use_thinking,
-    use_memory=prompt_flags.use_memory,
-    use_concrete_example=prompt_flags.use_concrete_example,
-    use_abstract_example=prompt_flags.use_abstract_example,
-    use_hints=prompt_flags.use_hints,
-    enable_chat=prompt_flags.enable_chat,
-    max_prompt_tokens=prompt_flags.max_prompt_tokens,
-    be_cautious=prompt_flags.be_cautious,
-    extra_instructions=prompt_flags.extra_instructions,
+    use_plan=BASE_FLAGS.use_plan,
+    use_criticise=BASE_FLAGS.use_criticise,
+    use_thinking=BASE_FLAGS.use_thinking,
+    use_memory=BASE_FLAGS.use_memory,
+    use_concrete_example=BASE_FLAGS.use_concrete_example,
+    use_abstract_example=BASE_FLAGS.use_abstract_example,
+    use_hints=BASE_FLAGS.use_hints,
+    enable_chat=BASE_FLAGS.enable_chat,
+    max_prompt_tokens=BASE_FLAGS.max_prompt_tokens,
+    be_cautious=BASE_FLAGS.be_cautious,
+    extra_instructions=BASE_FLAGS.extra_instructions,
 )
 
 agent_config = HPAAgentArgs(
